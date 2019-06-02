cfcd2cef5f120a107c531d9b36652d535a18cfd7
zytzrh <44763952+zytzrh@users.noreply.github.com>
2019-06-02 20:33:09 +0800

    Update constant.h

--- back_end/constant.h
+++ back_end/constant.h
@@ -41,7 +41,7 @@
 #define  FILENAME 100
 
 //用户缓存区大小
-#define  UBSIZE 100
+#define  UBSIZE 100000
 
 //车站缓存区大小
 #define  STBSIZE 300
@@ -52,7 +52,7 @@
 
 #define  TicketBppSIZE 4096
 
-#define  BUFFERSIZE  500
+#define  BUFFERSIZE  500000
 
 #define   REMAINSIZE 800
 
